5b0dd6865d85b956205837e45c844805d30bfd2e
angang <angang@yiguo.com>
2017-11-30 10:30:11 +0800

    修复SqlBuilder添加多个参数，只赋值个别参数报错bug

--- Trolley/SqlBuilder.cs
+++ Trolley/SqlBuilder.cs
@@ -17,15 +17,34 @@ namespace Trolley
         private List<object> entityParameters = new List<object>();
         internal Dictionary<string, object> Parameters => this.parameters;
         internal List<object> EntityParameters => this.entityParameters;
+        /// <summary>
+        /// 用默认的Provider生成SqlBuilder对象
+        /// </summary>
         public SqlBuilder()
         {
             this.provider = OrmProviderFactory.DefaultProvider;
         }
+        /// <summary>
+        /// 使用指定的Provider生成SqlBuilder对象
+        /// </summary>
+        /// <param name="provider">Provider对象</param>
         public SqlBuilder(IOrmProvider provider)
         {
             this.provider = provider;
         }
+        /// <summary>
+        /// 追加sql语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
         public SqlBuilder RawSql(string sql) => this.RawSql(sql, null);
+        /// <summary>
+        /// 追加sql语句。
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="sql">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder RawSql(string sql, params object[] parameters)
         {
             if (String.IsNullOrEmpty(sql)) throw new ArgumentNullException("sql", "sql不能为空字符串！");
@@ -33,7 +52,22 @@ namespace Trolley
             this.AddParameters(sql, parameters);
             return this;
         }
+        /// <summary>
+        /// 追加AND子句。
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder AndWhere(string clause, params object[] parameters) => this.AndWhere(true, clause, parameters);
+        /// <summary>
+        /// 条件condition为true，追加AND子句，为false不追加AND子句。
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="condition">条件</param>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder AndWhere(bool condition, string clause, params object[] parameters)
         {
             if (condition)
@@ -49,7 +83,22 @@ namespace Trolley
             }
             return this;
         }
+        /// <summary>
+        /// 追加以(起始的AND子句。如： AND (...
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder AndWithParenthesis(string clause, params object[] parameters) => this.AndWithParenthesis(true, clause, parameters);
+        /// <summary>
+        /// 条件condition为true，追加以(起始的AND子句，为false不追加。如： AND (...。
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="condition">条件</param>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder AndWithParenthesis(bool condition, string clause, params object[] parameters)
         {
             if (condition)
@@ -65,12 +114,29 @@ namespace Trolley
             }
             return this;
         }
+        /// <summary>
+        /// 追加)
+        /// </summary>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder EndParenthesis()
         {
             this.sqlBuilder.Append(")");
             return this;
         }
+        /// <summary>
+        /// 追加OR子句。如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder OrWhere(string clause, params object[] parameters) => this.OrWhere(true, clause, parameters);
+        /// <summary>
+        /// 条件condition为true，追加OR子句，为false不追加OR子句。
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder OrWhere(bool condition, string clause, params object[] parameters)
         {
             if (condition)
@@ -81,7 +147,22 @@ namespace Trolley
             }
             return this;
         }
+        /// <summary>
+        /// 追加以(起始的OR子句。如： OR (...。
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder OrWithParenthesis(string clause, params object[] parameters) => this.OrWithParenthesis(true, clause, parameters);
+        /// <summary>
+        /// 条件condition为true，追加以(起始的OR子句，为false不追加子句。如： OR (...。
+        /// 如果有参数，则按照先后顺序把sql中用到的参数追加到sql命令的参数列表中。
+        /// </summary>
+        /// <param name="condition">条件</param>
+        /// <param name="clause">前面生成的参数</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder OrWithParenthesis(bool condition, string clause, params object[] parameters)
         {
             if (condition)
@@ -97,7 +178,20 @@ namespace Trolley
             }
             return this;
         }
+        /// <summary>
+        /// 追加字段名或是更新用字段名赋值子句。如："Column1","Column1,Column2","Column1=@value1"
+        /// </summary>
+        /// <param name="field">一个或多个列名称，多个以,分隔</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder AddField(string field, params object[] parameters) => this.AddField(true, field, parameters);
+        /// <summary>
+        /// 条件condition为true，追加字段名或是更新用字段名赋值子句，为false不追加。如："Column1","Column1,Column2","Column1=@value1"
+        /// </summary>
+        /// <param name="condition">条件</param>
+        /// <param name="field">一个或多个列名称，多个以,分隔</param>
+        /// <param name="parameters">sql中使用的参数值列表，顺序按照sql中出现的顺序</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder AddField(bool condition, string field, params object[] parameters)
         {
             if (condition)
@@ -113,27 +207,55 @@ namespace Trolley
             }
             return this;
         }
+        /// <summary>
+        /// 追加sql参数值
+        /// </summary>
+        /// <param name="name">参数名，包含@:?</param>
+        /// <param name="value">参数值</param>
+        /// <returns>返回SqlBuilder对象</returns>
         public SqlBuilder AddParameter(string name, object value)
         {
             if (this.parameters.ContainsKey(name)) return this;
             this.parameters.Add(name, value);
             return this;
         }
+        /// <summary>
+        /// 根据前面生成的sql，进行生成分页的总条数语句。
+        /// </summary>
+        /// <param name="pageIndex">返回数据的页索引，从0开始</param>
+        /// <param name="pageSize">每页的条数</param>
+        /// <param name="orderBy">排序语句</param>
+        /// <returns>返回生成的分页语句</returns>
         public string PagingTotal(int pageIndex, int pageSize, string orderBy = null)
         {
             var sql = this.sqlBuilder.ToString();
             return this.provider.GetPagingCountExpression(sql);
         }
+        /// <summary>
+        /// 根据前面生成的sql，进行生成分页语句，不包含分页的总条数语句。
+        /// </summary>
+        /// <param name="pageIndex">返回数据的页索引，从0开始</param>
+        /// <param name="pageSize">每页的条数</param>
+        /// <param name="orderBy">排序语句</param>
+        /// <returns>返回生成的分页语句</returns>
         public string Paging(int pageIndex, int pageSize, string orderBy = null)
         {
             var sql = this.sqlBuilder.ToString();
             return this.provider.GetPagingExpression(sql, pageIndex * pageSize, pageSize, orderBy);
         }
+        /// <summary>
+        /// 清空前面生成sql
+        /// </summary>
+        /// <returns>返回生成的分页语句</returns>
         public SqlBuilder Clear()
         {
             this.sqlBuilder.Clear();
             return this;
         }
+        /// <summary>
+        /// 生成sql并返回
+        /// </summary>
+        /// <returns>返回生成的sql</returns>
         public string BuildSql() => this.sqlBuilder.ToString();
         private void AddParameters(string clause, params object[] parameters)
         {
@@ -143,6 +265,7 @@ namespace Trolley
                 var index = 0;
                 for (int i = 0; i < matches.Count; i++)
                 {
+                    if (i >= parameters.Length) break;
                     if (this.Parameters.ContainsKey(matches[i].Value)) continue;
                     this.Parameters.Add(matches[i].Value, parameters[index]);
                     index++;
