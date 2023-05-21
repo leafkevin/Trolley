edf29cdc70999f9de065bfeee6561e4f0b67fa85
leafkevin <leafkevin@outlook.com>
2023-05-21 12:10:51 +0800

    支持MySql 5.7版本获取自增ID

--- src/Trolley.MySqlConnector/MySqlProvider.cs
+++ src/Trolley.MySqlConnector/MySqlProvider.cs
@@ -11,7 +11,7 @@ public partial class MySqlProvider : BaseOrmProvider
     private static Dictionary<Type, object> defaultDbTypes = new();
     private static Dictionary<Type, string> castTos = new();
 
-    public override string SelectIdentitySql => " RETURNING {0}";
+    public override string SelectIdentitySql => ";SELECT LAST_INSERT_ID()";
     public override Type NativeDbTypeType => typeof(MySqlDbType);
     static MySqlProvider()
     {
