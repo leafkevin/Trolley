667d14142b203d22ad7ebda3d3326ecaf9f6d16c
leafkevin <leafkevin@outlook.com>
2023-06-19 23:23:00 +0800

    update

--- src/nupkg/push.ps1
+++
@@ -1,8 +0,0 @@
-# 获取当前目录路径
-cd $PSScriptRoot
-$filelist=dir *.nupkg
-
-foreach($file in $filelist) {
-    dotnet nuget push $file -k oy2fcjf5ldovgdempxpq5aneean4lznepeel5corlghfsu -s https://api.nuget.org/v3/index.json
-}
-pause
